532730ce7b834ee991faff8a614aa3a880df8cb8
elenagolovko <airdragon220@gmail.com>
2018-08-27 08:57:04 +0300

    new structure and sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require("gulp");
 var cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
 
-var concatCss = require('gulp-concat-css');
+// var concatCss = require('gulp-concat-css');
 
-gulp.task('default', function () {
-  return gulp.src('project/**/*.css')
-    .pipe(concatCss("styles/bundle.css"))
+// gulp.task('default', function () {
+//   return gulp.src('project/**/*.css')
+//     .pipe(concatCss("styles/bundle.css"))
+//     .pipe(cleanCSS())
+//     .pipe(gulp.dest('css/'));
+// });
+
+gulp.task('default', function(){
+  return gulp.src('./sass/**/*.{scss,sass}')
+    .pipe(sass())
     .pipe(cleanCSS())
     .pipe(gulp.dest('css/'));
 });

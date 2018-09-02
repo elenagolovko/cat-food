a1d45e55829d2c88da17e8e57e3681cfbef5e782
elenagolovko <airdragon220@gmail.com>
2018-09-02 21:29:09 +0300

    card states: text change

--- js/product-card_states.js
+++ js/product-card_states.js
@@ -10,17 +10,27 @@
     elem.classList.toggle(secondClass);
   };
 
+  var toggleElems = function (elem1, elem2, className) {
+    elem1.classList.toggle(className);
+    elem2.classList.toggle(className);
+  };
+
   function handleCardButtonClick(evt) {
     var element = evt.target.parentNode.previousElementSibling;
-    toggleClasses(element, 'product-card--selected', 'product-card--default');
+    toggleElems(element, element.nextElementSibling, 'visually-hidden');
+    toggleClasses(element.previousElementSibling, 'product-card--selected', 'product-card--default');
   }
 
   function handleCardClick(evt) {
+    var description = evt.target.nextElementSibling;
+    toggleElems(description, description.nextElementSibling, 'visually-hidden');
     toggleClasses(evt.target, 'product-card--selected', 'product-card--default');
   }
 
   function handleCardKeyDown(evt) {  
     if (evt.keyCode === KEY_CODE.ENTER) {
+      var description = this.nextElementSibling;
+      toggleElems(description, description.nextElementSibling, 'visually-hidden');
       toggleClasses(this, 'product-card--selected', 'product-card--default');
     }
   }
